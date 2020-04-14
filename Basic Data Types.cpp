5398562242a5d2cbfc192c4b3c6c0db57a79159a
Saroar Zahan Sojib <61473634+Saroar-Sojib@users.noreply.github.com>
2020-04-14 13:09:39 +0600

    Easy

---
+++ Basic Data Types.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    long long a;
+    char s;
+    float m;
+    double e;
+    cin>>n>>a>>s>>m>>e;
+    cout<<n<<endl;
+    cout<<a<<endl;
+    cout<<s<<endl;
+    printf("%.3f\n",m);
+    printf("%.9lf\n",e);
+    return 0;
+}
